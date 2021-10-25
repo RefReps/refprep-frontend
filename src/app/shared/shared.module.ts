09e769862551f9ac6a7e95b5f54058b132e0826a
Hunter Durbin <hmdurbin@bsu.edu>
2021-10-24 20:35:44 -0400

    Add Shared components

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatIconModule } from '@angular/material/icon';
+import { SidebarIconComponent } from '../components/sidebar-icon/sidebar-icon.component';
+import { ComponentsModule } from '../components/components.module';
+
+
+@NgModule({
+  declarations: [ // Declare all components in the shared/components
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule,
+    MatIconModule,
+    
+    ComponentsModule
+
+  ],
+  exports: [ // Export all components in the shared/components
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+  ]
+})
+export class SharedModule { }
