72a41ed4e5c7b5cbdff9ed0442438775e191728b
Annie Peabody <aepeabody@bsu.edu>
2021-10-27 22:31:02 -0400

    add async test helpers

--- src/service/api.service.spec.ts
+++ src/service/api.service.spec.ts
@@ -1,7 +1,6 @@
-import { TestBed } from '@angular/core/testing';
-import { of } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
+import { asyncData, asyncError } from 'src/async-observable-helpers';
 import { CourseInfo } from 'src/app/models/course-info';
-
 import { ApiService } from './api.service';
 
 describe('ApiService', () => {
@@ -14,11 +13,10 @@ describe('ApiService', () => {
     apiService = new ApiService(httpClientSpy as any);
   });
 
-  it('should return expected courses (HttpClient called once)', (done: DoneFn) => {
+  it('should return expected courses', (done: DoneFn) => {
     const expectedCourses: CourseInfo[] =
-    [{"_id":"61715885a88ae2f163083ebf","courseName":"Baseball 101"},{"_id":"6172bc7fcd8d63270bfdf43b","courseName":"Course Not Named"},{"_id":"6175aa47af8840e6d8543d4d","courseName":"Basketball 101"},{"_id":"61770c9a6f259d21318d0867","courseName":"Basketball 101"}];
-    
-    httpClientSpy.get.and.returnValue(of(expectedCourses));
+      [{_id:"61793332928a024f0a2ba021", courseName:"Course Not Named"},{_id:"61793356928a024f0a2ba025", courseName:"Course 1"},{_id:"61793375928a024f0a2ba02a", courseName:"Course 2"}];    
+    httpClientSpy.get.and.returnValue(asyncData(expectedCourses));
 
     apiService.getAllCoursesInfo().subscribe(
       courses => {
@@ -28,6 +26,23 @@ describe('ApiService', () => {
       done.fail
     );
     expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
+  });
+
+  it('should return an error when the server returns a 404', (done: DoneFn) => {
+    const errorResponse = new HttpErrorResponse({
+      error: 'test 404 error',
+      status: 404, statusText: 'Not Found'
+    });
+
+    httpClientSpy.get.and.returnValue(asyncError(errorResponse));
+
+    apiService.getAllCoursesInfo().subscribe(
+      courses => done.fail('error expected'),
+      error => {
+        expect(error.message).toContain('404 Not Found');
+        done();
+      }
+    );
   });
 
   it('should be created', () => {
