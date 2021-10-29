654564fa300e83e1c126894c3f40ec035a2eecb2
Annie Peabody <aepeabody@bsu.edu>
2021-10-28 20:09:15 -0400

    add Api Service tests

--- src/service/api.service.spec.ts
+++ src/service/api.service.spec.ts
@@ -1,49 +1,42 @@
-import { HttpErrorResponse } from '@angular/common/http';
-import { asyncData, asyncError } from 'src/async-observable-helpers';
-import { CourseInfo } from 'src/app/models/course-info';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { ApiService } from './api.service';
+import { TestBed } from '@angular/core/testing';
+import { CourseBreifInfo } from 'src/app/models/course-breif-info';
 
 describe('ApiService', () => {
-  let httpClientSpy: { get: jasmine.Spy };
+  let httpTestingController: HttpTestingController;
   let apiService: ApiService;
 
   beforeEach(() => {
-    //TODO: spy on other methods too
-    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get'])
-    apiService = new ApiService(httpClientSpy as any);
-  });
-
-  it('should return expected courses', (done: DoneFn) => {
-    const expectedCourses: CourseInfo[] =
-      [{_id:"61793332928a024f0a2ba021", courseName:"Course Not Named"},{_id:"61793356928a024f0a2ba025", courseName:"Course 1"},{_id:"61793375928a024f0a2ba02a", courseName:"Course 2"}];    
-    httpClientSpy.get.and.returnValue(asyncData(expectedCourses));
-
-    apiService.getAllCoursesInfo().subscribe(
-      courses => {
-        expect(courses).toEqual(expectedCourses, 'expected courses');
-        done();
-      },
-      done.fail
-    );
-    expect(httpClientSpy.get.calls.count()).toBe(1, 'one call');
-  });
-
-  it('should return an error when the server returns a 404', (done: DoneFn) => {
-    const errorResponse = new HttpErrorResponse({
-      error: 'test 404 error',
-      status: 404, statusText: 'Not Found'
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
     });
 
-    httpClientSpy.get.and.returnValue(asyncError(errorResponse));
-
-    apiService.getAllCoursesInfo().subscribe(
-      courses => done.fail('error expected'),
-      error => {
-        expect(error.message).toContain('404 Not Found');
-        done();
-      }
-    );
+    httpTestingController = TestBed.get(HttpTestingController);
+    apiService = TestBed.get(ApiService);
   });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+  //need to make a dummy set of info to test against
+  it('getAllCoursesInfo() should return expected info', (done) => {
+    const expectedData: CourseBreifInfo[] = [
+      {'_id':'61793332928a024f0a2ba021','courseName':'Course Not Named'},
+      {'_id':'61793356928a024f0a2ba025','courseName':'Course 1'},
+      {'_id':'61793375928a024f0a2ba02a','courseName':'Course 2'}   
+    ];
+
+    apiService.getAllCoursesInfo().subscribe( data => {
+      expect(data).toEqual(expectedData);
+      done();
+    });
+
+    const testRequest = httpTestingController.expectOne('http://localhost:3000/api/course');
+    testRequest.flush(expectedData);
+  });
+
 
   it('should be created', () => {
     expect(apiService).toBeTruthy();
