57ac5c8c3c954dc493a531072e4561f6bbddeaef
Hunter Durbin <hmdurbin@bsu.edu>
2021-12-05 16:11:40 -0500

    Add new calls to api

--- src/service/api.service.ts
+++ src/service/api.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
-import { Video } from 'src/models/Video/video.model';
-import { CourseBreifInfo } from 'src/app/models/course-breif-info';
-import { CourseInfo } from 'src/app/models/course-info';
+import { Video } from 'src/app/models/video.model';
+import { Course } from 'src/app/models/course';
 import { SectionsInfo } from 'src/app/models/sections-info';
 import { ModuleInfo } from 'src/app/models/module-info';
 import { Content } from '@angular/compiler/src/render3/r3_ast';
+import { Module } from 'src/app/models/module';
+import { Section } from 'src/app/models/section';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +16,12 @@ import { Content } from '@angular/compiler/src/render3/r3_ast';
 export class ApiService {
   
   baseUrl: string = 'http://localhost:3000';
+  apiUrl: string = `${this.baseUrl}/api/`;
   videoUrl: string = `${this.baseUrl}/api/video`;
-  courseUrl: string = `${this.baseUrl}/api/course`
+  courseUrl: string = `${this.baseUrl}/api/course`;
+  sectionUrl: string = `${this.baseUrl}/api/section`;
+  moduleUrl: string = `${this.baseUrl}/api/module`;
+  contentUrl: string = `${this.baseUrl}/api/content`;
 
   constructor(private http: HttpClient) { }
 
@@ -48,24 +53,66 @@ export class ApiService {
     return this.http.get<Video[]>(`${this.baseUrl}?title=${title}`);
   }
 
-  getAllCoursesInfo(): Observable<CourseBreifInfo[]> {
-    return this.http.get<CourseBreifInfo[]>(`${this.courseUrl}`)
-  }
-
-  getCourseInfo(courseId: string): Observable<CourseInfo> {
-    return this.http.get<CourseInfo>(`${this.courseUrl}/${courseId}`)
-  }
-
+  
   getCourseSections(courseId: string): Observable<SectionsInfo[]>{
     return this.http.get<SectionsInfo[]>(`${this.courseUrl}/${courseId}/section`)
   }
-
+  
   getSectionModules(courseId: string, sectionId: string): Observable<ModuleInfo[]>{
     return this.http.get<ModuleInfo[]>(`${this.courseUrl}/${courseId}/section/${sectionId}/module`)
   }
-
+  
   getModuleContent(courseId: string, sectionId: string, moduleID: string): Observable<Content[]>{
     return this.http.get<Content[]>(`${this.courseUrl}/${courseId}/section/${sectionId}/module/${moduleID}/content`)
   }
+  
+  // Start of new calls //
+  
+  // Course Routes
+  
+  getAllCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(`${this.courseUrl}`)
+  }
+
+  getCourse(courseId: string): Observable<Course> {
+    return this.http.get<Course>(`${this.courseUrl}/${courseId}`)
+  }
+
+  postCourse(courseForm: FormData): void {
+    this.http.post(`${this.courseUrl}`, courseForm).subscribe();
+  }
+
+  updateCourse(courseId: string, courseForm: FormData): void {
+    this.http.put(`${this.courseUrl}/${courseId}`, courseForm).subscribe();
+  }
+
+  // Section Routes
+
+  getSections(courseId: string): Observable<Section[]> {
+    const query = `courseId=${courseId}`
+    return this.http.get<Section[]>(`${this.sectionUrl}?${query}`)
+  }
+
+  // Module Routes
+
+  getModules(sectionId: string): Observable<Module[]> {
+    const query = `sectionId=${sectionId}`
+    return this.http.get<Module[]>(`${this.moduleUrl}?${query}`)
+  }
+
+  // Content Routes
+
+  getContents(moduleId: string): Observable<Content[]> {
+    const query = `moduleId=${moduleId}`
+    return this.http.get<Content[]>(`${this.contentUrl}?${query}`)
+  }
+
+  // Video Routes
+
+  getVideoMetadata(videoId: string): Observable<Video> {
+    return this.http.get<Video>(`${this.videoUrl}/${videoId}/meta`)
+  }
+
+
 
 }
