478b750d34aa9f86be6dff07d28f78a6f30022c5
Hunter Durbin <hmdurbin@bsu.edu>
2021-12-05 16:10:44 -0500

    Add samples in the test page

--- src/app/modules/test-component/test-component.component.ts
+++ src/app/modules/test-component/test-component.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Course } from 'src/app/models/course';
 
 @Component({
   selector: 'app-test-component',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-component.component.css']
 })
 export class TestComponentComponent implements OnInit {
+
+  course: Course = {
+    "_id": "61a5685c462f70ef5a2b51f0",
+    "name": "Basketball 101",
+    "isTemplate": true,
+    "isPublished": true,
+    "isDeleted": false,
+  }
 
   constructor() { }
 
