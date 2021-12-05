f57762fd529376d0eafafd08981122a9d601fd15
Hunter Durbin <hmdurbin@bsu.edu>
2021-12-05 16:11:31 -0500

    Move old models

--- src/models/Content/content.ts
+++
@@ -1,6 +0,0 @@
-export class Content {
-    toContentId?: any;
-    name?: string;
-    order?: number;
-    type?: string;
-}
--- src/models/Video/video.model.ts
+++
@@ -1,5 +0,0 @@
-export class Video {
-    id?: any;
-    title?: string;
-    description?: string;
-}
