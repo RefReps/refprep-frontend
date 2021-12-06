77f9e665abc5e05fc0b80716eb195909ad322937
Hunter Durbin <hmdurbin@bsu.edu>
2021-12-05 22:26:22 -0500

    Cleanup

--- src/app/modules/course-home/course-home.component.ts
+++ src/app/modules/course-home/course-home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseAddFormComponent } from 'src/app/components/course-add-form/course-add-form.component';
 import { CourseBreifInfo } from 'src/app/models/course-breif-info';
+import { DialogService } from 'src/service/dialog.service';
 
 @Component({
   selector: 'app-course-home',
