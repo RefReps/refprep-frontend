7fc0946098246aa7dbd44b830956b026288ca945
Hunter Durbin <hmdurbin@bsu.edu>
2021-12-05 22:27:43 -0500

    Test components

--- src/app/modules/test-component/test-component.component.ts
+++ src/app/modules/test-component/test-component.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseAddFormComponent } from 'src/app/components/course-add-form/course-add-form.component';
+import { ModuleFormAddComponent } from 'src/app/components/module-form-add/module-form-add.component';
+import { SectionFormAddComponent } from 'src/app/components/section-form-add/section-form-add.component';
 import { Course } from 'src/app/models/course';
+import { DialogService } from 'src/service/dialog.service';
 
 @Component({
   selector: 'app-test-component',
@@ -16,9 +20,21 @@ export class TestComponentComponent implements OnInit {
     "isDeleted": false,
   }
 
-  constructor() { }
+  constructor(private dialogService: DialogService) { }
 
   ngOnInit(): void {
+  }
+
+  openAddCourseDialog() {
+    this.dialogService.open(CourseAddFormComponent, {})
+  }
+
+  openAddSectionDialog() {
+    this.dialogService.open(SectionFormAddComponent, {courseId: '123412341234123412341234'})
+  }
+
+  openAddModuleDialog() {
+    this.dialogService.open(ModuleFormAddComponent, {sectionId: '432143214321432143214321'})
   }
 
 }
