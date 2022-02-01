ef84d8782238966c6b7b009afcbafc052a132385
Hunter Durbin <hmdurbin@bsu.edu>
2022-01-31 21:08:46 -0500

    Route login/logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogoutComponent } from './components/logout/logout.component';
 import { VideoUploadComponent } from './components/video-upload/video-upload.component';
 import { DefaultComponent } from './layouts/default/default.component';
+import { LoginComponent } from './layouts/login/login.component';
 import { CourseHomeComponent } from './modules/course-home/course-home.component';
 import { CourseVideoComponent } from './modules/course-video/course-video.component';
 import { CoursesComponent } from './modules/courses/courses.component';
 import { EditCurriculumHomeComponent } from './modules/edit-curriculum-home/edit-curriculum-home.component';
 import { HomeComponent } from './modules/home/home.component';
 import { TestComponentComponent } from './modules/test-component/test-component.component';
+import { AuthGuard } from './_helpers/auth.guard';
 
 const routes: Routes = [ // Always put more specific routes on the top
+
   {
     path: '', 
     component: DefaultComponent, 
+    canActivate: [AuthGuard],
     children: [
       {path: '', redirectTo: 'home', pathMatch: 'full'}, 
       {path: 'courses/:courseId/video/:videoId', component: CourseVideoComponent},
@@ -26,8 +31,16 @@ const routes: Routes = [ // Always put more specific routes on the top
     ]
   },
   {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent,
+  },
+  {
     path: '**',
-    component: DefaultComponent,
+    component: LoginComponent,
   },
 ];
 
