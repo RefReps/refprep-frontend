a5575757a743d7269f23fe7d39ae9c3c31eee28d
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-01 18:01:17 -0500

    Remove old sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,34 +1,31 @@
 import { NgModule } from '@angular/core';
+
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from '@angular/router';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatIconModule } from '@angular/material/icon';
-import { SidebarIconComponent } from '../components/sidebar-icon/sidebar-icon.component';
 import { ComponentsModule } from '../components/components.module';
-import { MatCard } from '@angular/material/card';
-import { CourseHomeComponent } from '../modules/course-home/course-home.component';
-
+import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [ // Declare all components in the shared/components
     HeaderComponent,
-    SidebarComponent,
     FooterComponent,
   ],
   imports: [
     CommonModule,
     RouterModule,
     FlexLayoutModule,
-    MatIconModule,
     ComponentsModule,
-
+    MaterialModule,
+    FormsModule,
+    LayoutModule,
   ],
   exports: [ // Export all components in the shared/components
     HeaderComponent,
-    SidebarComponent,
     FooterComponent,
   ]
 })
