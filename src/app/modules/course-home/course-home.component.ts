95c9c2d8eabfa95aa87b09aede259af87455b996
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-06 16:30:20 -0500

    Set course on user when getting course

--- src/app/modules/course-home/course-home.component.ts
+++ src/app/modules/course-home/course-home.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from 'src/app/models/course';
 import { CourseBreifInfo } from 'src/app/models/course-breif-info';
+import { CourseInteractionService } from 'src/app/_services/course-interaction.service';
+import { UserInteractionService } from 'src/app/_services/user-interaction.service';
 import { ApiService } from 'src/service/api.service';
 
 @Component({
@@ -16,23 +18,31 @@ export class CourseHomeComponent implements OnInit {
 
   constructor(
     private Api: ApiService, 
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private userInteraction: UserInteractionService,
     ) { }
 
   ngOnInit(): void {
     this.route.paramMap
-      .subscribe(params => {
-        let id = params.get('courseId');
-        if (id) {
-          this.courseId = id;
-        }
-      })
+    .subscribe(params => {
+      let id = params.get('courseId');
+      if (id) {
+        this.courseId = id;
+      }
+    })
     this.getCourseInfo();
   }
-
+  
   getCourseInfo(): void {
     this.Api.getCourse(this.courseId)
-      .subscribe(info => this.course = info)
+    .subscribe(info => {
+      this.course = info
+      this.userInteraction.setCourse(this.course)
+    })
+  }
+
+  get canEdit(): boolean {
+    return this.userInteraction.isAuthor
   }
 
 }
