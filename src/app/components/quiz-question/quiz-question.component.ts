5dd2410705be5274b3ec6681367846a00d11df30
Hunter Durbin <hmdurbin@bsu.edu>
2022-02-07 22:47:46 -0500

    Add quiz taking

--- src/app/components/quiz-question/quiz-question.component.ts
+++ src/app/components/quiz-question/quiz-question.component.ts
@@ -1,65 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output } from '@angular/core';
 import { FormBuilder,FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Question } from 'src/app/models/question';
+import { QuizAnswerSaverService } from 'src/app/_services/quiz-answer-saver.service';
+import { ApiService } from 'src/service/api.service';
 
 @Component({
   selector: 'app-quiz-question',
   templateUrl: './quiz-question.component.html',
   styleUrls: ['./quiz-question.component.scss']
 })
-export class QuizQuestionComponent implements OnInit {
-   
+export class QuizQuestionComponent {
+   @Input() question: Question = {}
+   @Input() quizId: string = ''
 
-  questionList: Question[] = [
-    {questionNumber : 1,
-            "type": "1_CHOICE", 
-            "question": "What is my favorite color?",
-            "responses": {
-                "A": "Aqua",
-                "B": "Black",
-                "C": "Cream",
-                "D": "Dark Orange"
-            },
-            "answers": ["A", "C"], 
-        },
-        {questionNumber : 2, 
-            "type": "MULTI_CHOICE", 
-            "question": "What is my favorite colors?",
-            "responses": {
-                "A": "Blue",
-                "B": "Red",
-                "C": "Yellow",
-                "D": "Orange"
-            },
-            "answers": ["A", "B"], 
-        },
-        {questionNumber : 3,  
-            "type": "TRUE_FALSE", 
-            "question": "Blue is my favorite color.",
-            "answer": true, // true | false
-        },
-        {questionNumber : 4,
-            "type": "FREE_RESPONSE",
-            "question": "What is my favorite color?", 
-
-            "answers": ["green", "Green", "GREEN"], 
-            
-        }
-  ]
-  userAnswer = this.formBuilder.group({
-    question: [''],
-    answer: new FormControl('')
-  });
-
-  constructor(private formBuilder: FormBuilder) {     
-  }
-
-  ngOnInit(): void {
-    
+  // need to formate {"1": "Answer"}
+  constructor(
+    private quizAnswerService: QuizAnswerSaverService,
+    private router: Router,
+    private api: ApiService,
+  ) {   
   }
   
-  onSubmit() {
-    console.warn(this.userAnswer.value)
+  ngOnInit(): void { 
   }
+
+  updateAnswers(value: string) {
+    let answer: any = {[this.question.questionNumber!]: value}
+    console.log(answer)
+    this.quizAnswerService.saveAnswer(answer)
+  }
+
+  submitQuiz(): void  {
+    if (this.quizId) {
+      this.api.submissionSave(this.quizId, this.quizAnswerService.getAnswers()).subscribe(data => {
+        this.api.gradeQuiz(this.quizId).subscribe(data => {
+          console.log(data)
+        })
+      })
+    }
+  }
+  
+
  
 }
--- src/app/components/quiz-student-view/quiz-student-view.component.ts
+++ src/app/components/quiz-student-view/quiz-student-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Question } from 'src/app/models/question';
+import { QuizAnswerSaverService } from 'src/app/_services/quiz-answer-saver.service';
 
 
 @Component({
@@ -9,17 +10,166 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class QuizStudentViewComponent implements OnInit {
 
+  questions: Question[] = [
+    {
+			"type": "1_CHOICE",
+			"questionNumber": 1,
+			"question": "What is my favorite color?",
+			"responses": {
+				"A": "Red",
+				"B": "Blue",
+				"C": "Cream",
+				"D": "Black"
+			},
+			"answers": ["B"]
+		},
+		{
+			"type": "TRUE_FALSE",
+			"questionNumber": 2,
+			"question": "This answer is false.",
+			"responses": {
+				"A": "A",
+				"B": "B",
+				"C": "C",
+				"D": "D"
+			},
+			"answer": false
+		},
+		{
+			"type": "FREE_RESPONSE",
+			"questionNumber": 3,
+			"question": "Enter Sandman",
+			"answers": ["Sandman", "sandman", "SANDMAN"]
+		},
+    {
+			"type": "1_CHOICE",
+			"questionNumber": 1,
+			"question": "What is my favorite color?",
+			"responses": {
+				"A": "Red",
+				"B": "Blue",
+				"C": "Cream",
+				"D": "Black"
+			},
+			"answers": ["B"]
+		},
+		{
+			"type": "TRUE_FALSE",
+			"questionNumber": 2,
+			"question": "This answer is false.",
+			"responses": {
+				"A": "A",
+				"B": "B",
+				"C": "C",
+				"D": "D"
+			},
+			"answer": false
+		},
+		{
+			"type": "FREE_RESPONSE",
+			"questionNumber": 3,
+			"question": "Enter Sandman",
+			"answers": ["Sandman", "sandman", "SANDMAN"]
+		},
+    {
+			"type": "1_CHOICE",
+			"questionNumber": 1,
+			"question": "What is my favorite color?",
+			"responses": {
+				"A": "Red",
+				"B": "Blue",
+				"C": "Cream",
+				"D": "Black"
+			},
+			"answers": ["B"]
+		},
+		{
+			"type": "TRUE_FALSE",
+			"questionNumber": 2,
+			"question": "This answer is false.",
+			"responses": {
+				"A": "A",
+				"B": "B",
+				"C": "C",
+				"D": "D"
+			},
+			"answer": false
+		},
+		{
+			"type": "FREE_RESPONSE",
+			"questionNumber": 3,
+			"question": "Enter Sandman",
+			"answers": ["Sandman", "sandman", "SANDMAN"]
+		},
+    {
+			"type": "1_CHOICE",
+			"questionNumber": 1,
+			"question": "What is my favorite color?",
+			"responses": {
+				"A": "Red",
+				"B": "Blue",
+				"C": "Cream",
+				"D": "Black"
+			},
+			"answers": ["B"]
+		},
+		{
+			"type": "TRUE_FALSE",
+			"questionNumber": 2,
+			"question": "This answer is false.",
+			"responses": {
+				"A": "A",
+				"B": "B",
+				"C": "C",
+				"D": "D"
+			},
+			"answer": false
+		},
+		{
+			"type": "FREE_RESPONSE",
+			"questionNumber": 3,
+			"question": "Enter Sandman",
+			"answers": ["Sandman", "sandman", "SANDMAN"]
+		},
+    {
+			"type": "1_CHOICE",
+			"questionNumber": 1,
+			"question": "What is my favorite color?",
+			"responses": {
+				"A": "Red",
+				"B": "Blue",
+				"C": "Cream",
+				"D": "Black"
+			},
+			"answers": ["B"]
+		},
+		{
+			"type": "TRUE_FALSE",
+			"questionNumber": 2,
+			"question": "This answer is false.",
+			"responses": {
+				"A": "A",
+				"B": "B",
+				"C": "C",
+				"D": "D"
+			},
+			"answer": false
+		},
+		{
+			"type": "FREE_RESPONSE",
+			"questionNumber": 3,
+			"question": "Enter Sandman",
+			"answers": ["Sandman", "sandman", "SANDMAN"]
+		},
+  ]
  
 
-  constructor() {
-    ({
-
-    })
-   }
+  constructor(
+    private quizAnswersSaver: QuizAnswerSaverService,
+  ) { }
 
 
   ngOnInit(): void {
-
   }
 
 }
