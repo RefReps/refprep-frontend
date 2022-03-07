a3801253855b9a798a9208792bc1fc365b84e30f
Hunter Durbin <hmdurbin@bsu.edu>
2022-03-07 16:51:18 -0500

    Revise course model

--- src/app/models/course.ts
+++ src/app/models/course.ts
@@ -1,11 +1,48 @@
-import { Settings } from "./settings";
-
 export class Course {
-    _id?: string;
-    name?: string;
-    isTemplate?: boolean;
-    isPublished?: boolean;
-    isDeleted?: boolean;
-    settings?: Settings;
-    isAuthor?: boolean;
+  _id?: string;
+  name?: string;
+  isTemplate?: boolean;
+  isPublished?: boolean;
+  isDeleted?: boolean;
+  settings?: Settings;
+  isAuthor?: boolean; // do i need?
+  // authors?: Author[];
+  //   students?: Student[];
+  sections?: Section[] | string[];
+}
+export class Settings {
+  isEnforcements?: boolean;
+  enforcementPercent?: number;
+  isGradedQuizAdvance?: boolean;
+  maximumQuizAttempts?: number;
+}
+
+export class Section {
+  _id!: string;
+  name?: string;
+  courseId?: string;
+  isPublished?: boolean;
+  sectionOrder?: number;
+  dropDate?: Date;
+  modules?: Module[] | string[];
+}
+
+export class Module {
+  _id!: string;
+  name?: string;
+  sectionId?: string;
+  isPublished?: boolean;
+  moduleOrder?: number;
+  dropDate?: Date;
+  contents?: Content[] | string[];
+}
+export class Content {
+  _id?: string;
+  name?: string;
+  toDocument?: string;
+  onModel?: string;
+  moduleId?: string;
+  isPublished?: boolean;
+  contentOrder?: number;
+  dropDate?: Date;
 }
