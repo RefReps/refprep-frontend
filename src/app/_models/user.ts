9323aabbf1de969475cdd86b31a78f26874201e6
Hunter Durbin <hmdurbin@bsu.edu>
2022-03-07 17:31:36 -0500

    Remove old models

--- src/app/_models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    id?: number;
-    email?: string;
-    password?: string;
-    firstName?: string;
-    lastName?: string;
-    access_token?: string;
-    refresh_token?: string;
-}
