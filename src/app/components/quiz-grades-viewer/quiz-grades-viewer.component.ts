f9e64db0ba177c598e03f62c09f0ff0ae7be6319
Hunter Durbin <hmdurbin@bsu.edu>
2022-03-07 19:06:18 -0500

    Change api routes to quiz routes

--- src/app/components/quiz-grades-viewer/quiz-grades-viewer.component.ts
+++ src/app/components/quiz-grades-viewer/quiz-grades-viewer.component.ts
@@ -1,49 +1,39 @@
-import { DataSource } from '@angular/cdk/collections';
-import { DatePipe } from '@angular/common';
-import { Content } from '@angular/compiler/src/render3/r3_ast';
+import { QuizzesService } from './../../_services/quizzes.service';
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Sort } from '@angular/material/sort';
-import { Observable, ReplaySubject } from 'rxjs';
-import { ActiveVersion, Quiz } from 'src/app/models/quiz';
+import { ActiveVersion } from 'src/app/models/quiz';
 import { UserGrade } from 'src/app/models/quiz';
-import { UserInteractionService } from 'src/app/_services/user-interaction.service';
 import { ApiService } from 'src/service/api.service';
-import { QuizVersion } from 'src/app/models/quiz';
 
 @Component({
   selector: 'app-quiz-grades-viewer',
   templateUrl: './quiz-grades-viewer.component.html',
-  styleUrls: ['./quiz-grades-viewer.component.scss']
+  styleUrls: ['./quiz-grades-viewer.component.scss'],
 })
 export class QuizGradesViewerComponent implements OnInit {
-
   @Input() quizId: string = '';
   userGrades: UserGrade[] = [];
   quizInfo: ActiveVersion = {};
   highestGrades: UserGrade[] = [];
 
-  constructor(
-    private Api: ApiService,
-  ) { }
+  constructor(private Api: ApiService, private QuizService: QuizzesService) {}
 
   ngOnInit(): void {
-    this.getQuizName()
-    this.getQuizGrades()
+    this.getQuizName();
+    this.getQuizGrades();
   }
 
   getQuizName() {
-    this.Api.getQuizInfo(this.quizId)
-      .subscribe(info => {
-        this.quizInfo = info
-      })
+    this.QuizService.getActiveQuiz(this.quizId).subscribe((info) => {
+      this.quizInfo = info;
+    });
   }
 
   getQuizGrades(): void {
-    this.Api.getAllQuizGrades(this.quizId)
-      .subscribe(info => {
-        this.userGrades = info
-      })
+    this.QuizService.getAllQuizGrades(this.quizId).subscribe((info) => {
+      console.log(info || 'hi');
+      this.userGrades = info;
+    });
   }
 
   sortGrades(sort: Sort) {
@@ -62,8 +52,7 @@ export class QuizGradesViewerComponent implements OnInit {
         case 'grade':
           if (a.email == b.email) {
             return compare(a.grade, b.grade, isDesc);
-          }
-          else {
+          } else {
             return compare(a.email, b.email, isAsc);
           }
         case 'dateStarted':
@@ -76,64 +65,53 @@ export class QuizGradesViewerComponent implements OnInit {
     });
   }
 
-
   getGradePercentage(stringDecGrade: Number | undefined) {
-    var percentGrade: Number = Number(stringDecGrade) * 100
-    var stringPercentGrade: String = percentGrade.toFixed(2)
-    return stringPercentGrade
+    var percentGrade: Number = Number(stringDecGrade) * 100;
+    var stringPercentGrade: String = percentGrade.toFixed(2);
+    return stringPercentGrade;
   }
 
   getLetterGrade(stringDecGrade: Number | undefined) {
-    var percentGrade: Number = Number(stringDecGrade) * 100
-    var letterGrade: String
+    var percentGrade: Number = Number(stringDecGrade) * 100;
+    var letterGrade: String;
     if (percentGrade >= 97 && percentGrade <= 100) {
-      return letterGrade = "A+"
-    }
-    else if (percentGrade >= 93 && percentGrade <= 96.99) {
-      return letterGrade = "A"
-    }
-    else if (percentGrade >= 90 && percentGrade <= 92.99) {
-      return letterGrade = "A-"
-    }
-    else if (percentGrade >= 87 && percentGrade <= 89.99) {
-      return letterGrade = "B+"
-    }
-    else if (percentGrade >= 83 && percentGrade <= 86.99) {
-      return letterGrade = "B"
-    }
-    else if (percentGrade >= 80 && percentGrade <= 82.99) {
-      return letterGrade = "B-"
-    }
-    else if (percentGrade >= 77 && percentGrade <= 79.99) {
-      return letterGrade = "C+"
-    }
-    else if (percentGrade >= 73 && percentGrade <= 76.99) {
-      return letterGrade = "C"
-    }
-    else if (percentGrade >= 70 && percentGrade <= 72.99) {
-      return letterGrade = "C-"
-    }
-    else if (percentGrade >= 67 && percentGrade <= 69.99) {
-      return letterGrade = "D+"
-    }
-    else if (percentGrade >= 63 && percentGrade <= 66.99) {
-      return letterGrade = "D"
-    }
-    else if (percentGrade >= 60 && percentGrade <= 62.99) {
-      return letterGrade = "D-"
-    }
-    else {
-      return letterGrade = "F"
+      return (letterGrade = 'A+');
+    } else if (percentGrade >= 93 && percentGrade <= 96.99) {
+      return (letterGrade = 'A');
+    } else if (percentGrade >= 90 && percentGrade <= 92.99) {
+      return (letterGrade = 'A-');
+    } else if (percentGrade >= 87 && percentGrade <= 89.99) {
+      return (letterGrade = 'B+');
+    } else if (percentGrade >= 83 && percentGrade <= 86.99) {
+      return (letterGrade = 'B');
+    } else if (percentGrade >= 80 && percentGrade <= 82.99) {
+      return (letterGrade = 'B-');
+    } else if (percentGrade >= 77 && percentGrade <= 79.99) {
+      return (letterGrade = 'C+');
+    } else if (percentGrade >= 73 && percentGrade <= 76.99) {
+      return (letterGrade = 'C');
+    } else if (percentGrade >= 70 && percentGrade <= 72.99) {
+      return (letterGrade = 'C-');
+    } else if (percentGrade >= 67 && percentGrade <= 69.99) {
+      return (letterGrade = 'D+');
+    } else if (percentGrade >= 63 && percentGrade <= 66.99) {
+      return (letterGrade = 'D');
+    } else if (percentGrade >= 60 && percentGrade <= 62.99) {
+      return (letterGrade = 'D-');
+    } else {
+      return (letterGrade = 'F');
     }
   }
-
 }
 
-function compare(a: string | Number |undefined, b: string | Number | undefined, isAsc: boolean) {
-  if (typeof a != "undefined" && typeof b != "undefined") {
+function compare(
+  a: string | Number | undefined,
+  b: string | Number | undefined,
+  isAsc: boolean
+) {
+  if (typeof a != 'undefined' && typeof b != 'undefined') {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-  }
-  else {
-    return 0
+  } else {
+    return 0;
   }
 }
