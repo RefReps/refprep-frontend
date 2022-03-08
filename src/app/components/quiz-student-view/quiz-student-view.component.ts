ff0ad431d4612cb21e16a890d2fee3685817651b
Annie Peabody <aepeabody@bsu.edu>
2022-03-07 21:05:24 -0500

    change from apiservice to quizzesService

--- src/app/components/quiz-student-view/quiz-student-view.component.ts
+++ src/app/components/quiz-student-view/quiz-student-view.component.ts
@@ -12,16 +12,16 @@ import { ApiService } from 'src/service/api.service';
   styleUrls: ['./quiz-student-view.component.scss']
 })
 export class QuizStudentViewComponent implements OnInit {
-  quizId: string = ''
-  questions: QuizQuestion[] = []
+  quizId: string = '';
+  submissionId: string = '';
+  questions: QuizQuestion[] = [];
  
 
   constructor(
     private quizAnswerService: QuizAnswerSaverService,
-	private quizService: QuizzesService,
-  private api: ApiService,
-  private router: Router,
-  private route: ActivatedRoute,
+	  private quizService: QuizzesService,
+    private router: Router,
+    private route: ActivatedRoute,
   ) { }
 
 
@@ -34,18 +34,25 @@ export class QuizStudentViewComponent implements OnInit {
       }
     })
 
+    this.route.paramMap
+    .subscribe(params => {
+      let subid = params.get('submissionId');
+      if(subid) {
+        this.submissionId = subid
+      }
+    })
+
 	  if (this.quizId) {
 		  this.quizService.startQuiz(this.quizId).subscribe(data => {
         this.questions = data.quizQuestions
-        console.log(this.questions)
 		  })
 	  }
   }
 
   submitQuiz(): void  {
     if (this.quizId) {
-      this.api.submissionSave(this.quizId, this.quizAnswerService.getAnswers()).subscribe(data => {
-        this.api.gradeQuiz(this.quizId).subscribe(data => {
+      this.quizService.saveUserQuizAnswers(this.quizId, this.submissionId, this.questions).subscribe(data => {
+        this.quizService.finishSubmission(this.quizId, this.submissionId).subscribe(data => {
           this.route.paramMap
           .subscribe(params => {
             let id = params.get('courseId');
