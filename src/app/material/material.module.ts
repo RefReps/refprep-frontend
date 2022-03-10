724279ca64cef40530a9b90532719b54f5f22a7d
Hunter Durbin <hmdurbin@bsu.edu>
2022-03-10 17:55:13 -0500

    Add snackBarModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -14,46 +14,47 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from "@angular/material/core";
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatStepperModule } from '@angular/material/stepper';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatTableModule} from '@angular/material/table';
-import { MatSortModule } from "@angular/material/sort";
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
-    exports: [
-        MatCardModule,
-        MatIconModule,
-        MatDividerModule,
-        MatExpansionModule,
-        MatToolbarModule,
-        MatButtonModule,
-        MatInputModule,
-        MatFormFieldModule,
-        MatProgressBarModule,
-        MatListModule,
-        MatSelectModule,
-        MatDialogModule,
-        MatSlideToggleModule,
-        MatDatepickerModule,
-        MatNativeDateModule,
-        MatSidenavModule,
-        MatCheckboxModule,
-        MatGridListModule,
-        MatTooltipModule,
-        MatRadioModule,
-        MatTabsModule,
-        MatStepperModule,
-        MatMenuModule,
-        MatTableModule,
-        MatSortModule,
-    ]
+  exports: [
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatProgressBarModule,
+    MatListModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatSlideToggleModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatTooltipModule,
+    MatRadioModule,
+    MatTabsModule,
+    MatStepperModule,
+    MatMenuModule,
+    MatTableModule,
+    MatSortModule,
+    MatSnackBarModule,
+  ],
 })
-export class MaterialModule { }
+export class MaterialModule {}
