30fedc9a919069406d6bb07450c805b44eee696c
Annie Peabody <aepeabody@bsu.edu>
2022-03-11 20:02:32 -0500

    add to view graded quiz component

--- src/app/components/view-graded-quiz/view-graded-quiz.component.ts
+++ src/app/components/view-graded-quiz/view-graded-quiz.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { QuizQuestion } from 'src/app/models/quiz';
+import { QuizzesService } from 'src/app/_services/quizzes.service';
+import { saveQuizSubmissionId } from 'src/app/_store/quizAnswer/quizAnswer.action';
 
 @Component({
   selector: 'app-view-graded-quiz',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-graded-quiz.component.scss']
 })
 export class ViewGradedQuizComponent implements OnInit {
+  quizId: string = '';
+  questions: QuizQuestion[] = [];
+  userAnswers?: QuizQuestion[] = [];
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private quizService: QuizzesService,
+    private store: Store,
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      let id = params.get('quizId');
+      if (id) {
+        this.quizId = id;
+      }
+    });
+    if (this.quizId) {
+      this.quizService.startQuiz(this.quizId).subscribe((data) => {
+        this.questions = data.quizQuestions;
+        this.store.dispatch(
+          saveQuizSubmissionId({ id: data.quizSubmission._id || '' })
+        );
+      });
+    }
+  //  this.getUserQuizAnswers()
   }
+
+  // getUserQuizAnswers() {
+  //   this.quizService.fetchUserQuizAnswers(this.quizId).subscribe(info => {
+  //     this.store.select(
+  //       selectQuizAnswers
+  //     )
+  //   })
+  // }
 
 }
