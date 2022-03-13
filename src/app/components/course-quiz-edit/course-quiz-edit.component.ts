ba5d31aae6c1d950efba70443a2f09193c680277
Hunter Durbin <hmdurbin@bsu.edu>
2022-03-13 13:26:32 -0400

    Add course sidebar

--- src/app/components/course-quiz-edit/course-quiz-edit.component.ts
+++ src/app/components/course-quiz-edit/course-quiz-edit.component.ts
@@ -1,14 +1,16 @@
-import { addQuestion, moveQuestion, clearQuestions } from './../../_store/quiz/quiz.actions';
+import {
+  addQuestion,
+  moveQuestion,
+  clearQuestions,
+} from './../../_store/quiz/quiz.actions';
 import { selectQuiz } from './../../_store/quiz/quiz.selector';
 import { Store } from '@ngrx/store';
 import { QuizzesService } from 'src/app/_services/quizzes.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Quiz, QuizQuestion } from 'src/app/models/quiz';
-import { ApiService } from 'src/service/api.service';
+import { Quiz } from 'src/app/models/quiz';
 import { DialogService } from 'src/service/dialog.service';
 import { QuizBuilderComponent } from '../quiz-builder/quiz-builder.component';
-import { moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-course-quiz-edit',
@@ -26,8 +28,9 @@ export class CourseQuizEditComponent implements OnInit, OnDestroy {
     private quizService: QuizzesService,
     private store: Store
   ) {}
+
   ngOnDestroy(): void {
-    this.store.dispatch(clearQuestions())
+    this.store.dispatch(clearQuestions());
   }
 
   ngOnInit(): void {
