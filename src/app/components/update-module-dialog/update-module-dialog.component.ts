ed3890d8b904bb17b62f5a61f847de30f4b53c74
mkidd04 <maxwell.kidd4@gmail.com>
2022-03-14 21:14:32 -0400

    update module name

--- src/app/components/update-module-dialog/update-module-dialog.component.ts
+++ src/app/components/update-module-dialog/update-module-dialog.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, Input, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Section } from 'src/app/models/course';
+import { ApiService } from 'src/service/api.service';
 
 @Component({
   selector: 'app-update-module-dialog',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-module-dialog.component.css']
 })
 export class UpdateModuleDialogComponent implements OnInit {
-
-  constructor() { }
+  @Input() sectionId: string = '';
+  sectionInfo: Section = {
+    _id: this.sectionId
+  };
+  constructor(
+    private api: ApiService,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
 
   ngOnInit(): void {
+  }
+
+  updateModuleName(name: string) {
+    if (this.data.data.moduleId){
+      const form = new FormData();
+      form.set("name", name);
+      this.api.updateModule(this.data.data.moduleId, form);
+      window.location.reload();
+    }
   }
 
 }
