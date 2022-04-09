1d99328c5d5c8e3104f6e7bd2c929ac6a9ee762d
Cecilist <jcecil239@gmail.com>
2022-04-08 22:25:23 -0400

    Added Snackbar for when student attemps to seek videos.

--- src/app/components/video-viewer/video-viewer.component.ts
+++ src/app/components/video-viewer/video-viewer.component.ts
@@ -3,8 +3,8 @@ import { Course } from './../../models/course';
 import { Component, Input, OnInit } from '@angular/core';
 import { Video } from 'src/app/models/video.model';
 import { ApiService } from 'src/service/api.service';
-import {VgApiService} from '@videogular/ngx-videogular/core'
 import { UserInteractionService } from 'src/app/_services/user-interaction.service';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-video-viewer',
@@ -22,12 +22,12 @@ export class VideoViewerComponent implements OnInit {
 
   course: Course = {}
 
-  // vgApi: VgApiService;
 
   constructor(
     private Api: ApiService,
     private route: ActivatedRoute,
     private userInteraction: UserInteractionService,
+    private _snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
@@ -85,7 +85,6 @@ export class VideoViewerComponent implements OnInit {
     if (this.enforcements == true && !this.isAuthor) {
       this.disableSeeking()
     } 
-    this.setCheckpoints()
     videoPlayer.load()
 
     videoPlayer.addEventListener('loadeddata', this.loadHighestWatchTime.bind(this))
@@ -131,21 +130,13 @@ export class VideoViewerComponent implements OnInit {
     var delta = videoPlayer.currentTime - this.watchedTime;
     if (delta > 0.01) {
       videoPlayer.currentTime = this.watchedTime;
+      this._snackBar.open("Video seeking currently disabled!", "Dismiss" , {duration:3000});
+      
     }
   }
 
   videoEnd(): void {
     this.updateProgress(100)
-  }
-
-  setCheckpoints(): void {
-    const videoPlayer = <HTMLVideoElement>document.getElementById('videoPlayer')
-    var counter = 1
-    videoPlayer.addEventListener('timeupdate', function(){
-    if ((Math.round(videoPlayer.currentTime)) == (Math.round(videoPlayer.duration*(.1*counter)))) {
-        counter++
-      }
-    });
   }
 
   get videoTitle(): string {
@@ -155,5 +146,6 @@ export class VideoViewerComponent implements OnInit {
   get isAuthor(): boolean {
     return this.userInteraction.isAuthor
   }
+
 
 }
