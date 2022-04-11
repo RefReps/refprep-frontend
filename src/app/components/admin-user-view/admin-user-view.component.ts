ca8667fd4a99bbd7dec0c3a92f83d2f5ff3c4bfa
Annie Peabody <aepeabody@bsu.edu>
2022-04-10 21:28:43 -0400

    sort user table by role

--- src/app/components/admin-user-view/admin-user-view.component.ts
+++ src/app/components/admin-user-view/admin-user-view.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/service/api.service';
 import { User } from 'src/app/models/user';
+import { Sort } from '@angular/material/sort';
 
 
 
@@ -24,8 +25,34 @@ export class AdminUserViewComponent implements OnInit {
     })
   }
 
+  sortUsers(sort: Sort) {
+    const data = this.users.slice();
+    if (!sort.active || sort.direction === '') {
+      this.users = data;
+      return;
+    }
+    this.users = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      const isDesc = sort.direction === 'desc';
+      switch (sort.active) {
+        case 'Role':
+          return compare(a.role, b.role, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+  }
 
-
-
+  function compare(
+    a: string | Number | undefined,
+    b: string | Number | undefined,
+    isAsc: boolean
+  ) {
+    if (typeof a != 'undefined' && typeof b != 'undefined') {
+      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+    } else {
+      return 0;
+    }
   }
 
