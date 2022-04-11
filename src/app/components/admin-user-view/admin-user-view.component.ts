cadd26d3fedc8889714db78767da87e80fdfdb13
Annie Peabody <aepeabody@bsu.edu>
2022-04-11 17:07:10 -0400

    show active user and add dialog routes

--- src/app/components/admin-user-view/admin-user-view.component.ts
+++ src/app/components/admin-user-view/admin-user-view.component.ts
@@ -7,6 +7,10 @@ import { DialogChangePasswordComponent } from '../dialog-change-password/dialog-
 import { DialogService } from 'src/service/dialog.service';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { DialogChangeEmailComponent } from '../dialog-change-email/dialog-change-email.component';
+import { DialogChangeNameComponent } from '../dialog-change-name/dialog-change-name.component';
+import { DialogChangeRoleComponent } from '../dialog-change-role/dialog-change-role.component';
+import { TokenService } from 'src/app/_services/token.service';
 
 
 
@@ -20,25 +24,39 @@ export class AdminUserViewComponent implements OnInit {
   @ViewChild('menuTrigger')
   menuTrigger!: MatMenuTrigger; 
   users: User[] = [];
+  activeUser: string = '';
   displayedColumns: string[] = ['email', 'firstName', 'lastName', 'role', 'Edit'];
   dataSource = new MatTableDataSource<User>();
   @ViewChild(MatSort, { static: true }) sort!: MatSort;
 
   constructor(
     private api: ApiService,
-    public dialogService: DialogService
+    public dialogService: DialogService,
+    public token: TokenService,
   ) {}
 
-  openDialog(user: User): void {
-    this.dialogService.open(DialogChangePasswordComponent, { user })
-  }
-
-
   ngOnInit(): void {
+    this.activeUser = this.token.getEmail();
     this.api.getAllUsers().subscribe((info) => {
       this.dataSource.data = info.users || [];
       this.dataSource.sort = this.sort;
     });
+  }
+
+  openUpdatePasswordDialog(user: User): void {
+    this.dialogService.open(DialogChangePasswordComponent, { user })
+  }
+
+  openUpdateEmailDialog(user: User): void {
+    this.dialogService.open(DialogChangeEmailComponent, { user })
+  }
+
+  openUpdateNameDialog(user: User): void {
+    this.dialogService.open(DialogChangeNameComponent, { user })
+  }
+
+  openUpdateRoleDialog(user: User): void {
+    this.dialogService.open(DialogChangeRoleComponent, { user })
   }
 
   applyFilter(event: Event) {
