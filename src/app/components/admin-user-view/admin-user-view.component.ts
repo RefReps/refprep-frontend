1efb6a44650d37d56d0fd9edc19bf9ce6e117f15
Annie Peabody <aepeabody@bsu.edu>
2022-04-10 22:53:00 -0400

    sort and filter user list

--- src/app/components/admin-user-view/admin-user-view.component.ts
+++ src/app/components/admin-user-view/admin-user-view.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiService } from 'src/service/api.service';
 import { User } from 'src/app/models/user';
-import { Sort } from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 
@@ -12,47 +13,55 @@ import { Sort } from '@angular/material/sort';
 })
 export class AdminUserViewComponent implements OnInit {
   users: User[] = [];
-  displayedColumns: string[] = ['email', 'F.Name', 'L.Name', 'Role', 'Edit'];
+  displayedColumns: string[] = ['email', 'firstName', 'lastName', 'role', 'Edit'];
+  dataSource = new MatTableDataSource<User>();
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
+
   constructor(
     private api: ApiService,
-  ) {
-   
-  }
+  ) {}
 
   ngOnInit(): void {
     this.api.getAllUsers().subscribe((info) => {
-      this.users = info.users || []
-    })
-  }
-
-  sortUsers(sort: Sort) {
-    const data = this.users.slice();
-    if (!sort.active || sort.direction === '') {
-      this.users = data;
-      return;
-    }
-    this.users = data.sort((a, b) => {
-      const isAsc = sort.direction === 'asc';
-      const isDesc = sort.direction === 'desc';
-      switch (sort.active) {
-        case 'Role':
-          return compare(a.role, b.role, isAsc);
-        default:
-          return 0;
-      }
+      this.dataSource.data = info.users || [];
+      this.dataSource.sort = this.sort;
     });
   }
+
+  // sortUsers(sort: Sort) {
+  //   const data = this.users.slice();
+  //   if (!sort.active || sort.direction === '') {
+  //     this.users = data;
+  //     return;
+  //   }
+  //   this.users = data.sort((a, b) => {
+  //     const isAsc = sort.direction === 'asc';
+  //     const isDesc = sort.direction === 'desc';
+  //     switch (sort.active) {
+  //       case 'Role':
+  //         return compare(a.role, b.role, isAsc);
+  //       default:
+  //         return 0;
+  //     }
+  //   });
+  // }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  function compare(
-    a: string | Number | undefined,
-    b: string | Number | undefined,
-    isAsc: boolean
-  ) {
-    if (typeof a != 'undefined' && typeof b != 'undefined') {
-      return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
-    } else {
-      return 0;
-    }
   }
+
+  // function compare(
+  //   a: string | Number | undefined,
+  //   b: string | Number | undefined,
+  //   isAsc: boolean
+  // ) {
+  //   if (typeof a != 'undefined' && typeof b != 'undefined') {
+  //     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
+  //   } else {
+  //     return 0;
+  //   }
+  // }
 
